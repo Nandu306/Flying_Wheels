d1bc5b24faafc0ecabbd339bddbd10d472b515d2
Nandu Rajagopala <nandu.rajagopala@cantab.net>
2021-07-24 19:09:36 +0200

    added filter feature to index page

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -4,6 +4,8 @@ class Bike < ApplicationRecord
 
   has_one_attached :photo
 
+  scope :filter_by_bike_category, -> (bike_category) { where bike_category: bike_category }
+
   validates :bike_category, presence: true
   validates :frame_size, presence: true
   validates :description, presence: true
