0960a32befef9df2ea0d9bbb1f69fed2c4033e7e
Nandu Rajagopala <nandu.rajagopala@cantab.net>
2021-07-25 10:20:19 +0200

    added a bookings feature

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+
+  def create
+    @bike = Bike.find(params[:bike_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.bike = @bike
+    authorize @booking
+
+    if @booking.save
+      redirect_to bikes_path, notice: "Booking successful"
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
